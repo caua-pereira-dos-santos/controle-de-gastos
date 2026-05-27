eea4a53a714e098b8b0b528881a15fc3d9103990
Caua Pereira dos Santos <cauaperreiradossantos@gmail.com>
2026-05-26 21:58:28 -0300

    fix: removido comentarios dos arquivos indicados como em erro pelo debud do neon

--- git-user-name.bat
+++ git-user-name.bat
@@ -1,4 +1,4 @@
-git config --global user.name "Cauã Pereira dos Santos"
+git config --global user.name "Caua Pereira dos Santos"
 
 
 git config --global user.email "cauaperreiradossantos@gmail.com"
